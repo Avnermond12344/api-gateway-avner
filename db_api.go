e5ea087c38c9aafd34e87ad126b21e8766f26a44
Avnermond12344 <avnerm@campus.technion.ac.il>
2026-01-07 09:36:20 +0200

    adding API for the DB

---
+++ db_api.go
@@ -0,0 +1,31 @@
+package main
+
+func getSchoolsList() []School {
+	return GetSchools() // Mock_db. We should Later change to DB.getSchoolsList()
+}
+
+func getGradesBySchoolID(schoolID string) []Grade {
+	return GetGradesBySchoolID(schoolID) // Mock_db. We should Later change to DB.getGradesList(schoolID)
+}
+
+func getEquipment(schoolID string, gradeID string) []Equipment {
+	return GetEquipmentList(schoolID, gradeID) // Mock_db. We should Later change to DB.getEquipmentList(schoolID, gradeID)
+}
+
+func getUserIDByCredentials(userName, password string) string {
+	// Mock_db. We should Later change to DB.getUserID(userName, password)
+	for _, user := range MockUsers {
+		if user.Username == userName && user.Password == password {
+			return user.UserID
+		}
+	}
+	return ""
+}
+
+func getCartByUserID(userID string) []CartEntry {
+	return MockCarts[userID] //Mock_db. We should later change to DB.GetCartContent(userID)
+}
+
+func saveCart(userID string, cart []CartEntry) {
+	MockCarts[userID] = cart // Mock_db. We should later change to DB.setNewCart(userID, cart)
+}
