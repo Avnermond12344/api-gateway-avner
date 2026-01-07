470f6bf71733f780ecc88e37ea4e4957f711ef1d
Avnermond12344 <avnerm@campus.technion.ac.il>
2026-01-07 09:58:06 +0200

    adding getUsernameFromUserID function

--- db_api.go
+++ db_api.go
@@ -4,7 +4,7 @@ func getSchoolsList() []School {
 	return GetSchools() // Mock_db. We should Later change to DB.getSchoolsList()
 }
 
-func getGradesBySchoolID(schoolID string) []Grade {
+func getGradesList(schoolID string) []Grade {
 	return GetGradesBySchoolID(schoolID) // Mock_db. We should Later change to DB.getGradesList(schoolID)
 }
 
@@ -16,6 +16,16 @@ func getUserIDByCredentials(userName, password string) string {
 	// Mock_db. We should Later change to DB.getUserID(userName, password)
 	for _, user := range MockUsers {
 		if user.Username == userName && user.Password == password {
+			return user.UserID
+		}
+	}
+	return ""
+}
+
+func getUsernameFromUserID(userID string) string {
+	// Mock_db. We should Later change to DB.IsValidUserID(userID)
+	for _, user := range MockUsers {
+		if user.UserID == userID {
 			return user.UserID
 		}
 	}
